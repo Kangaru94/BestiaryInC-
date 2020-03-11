2788ac644705342f4a5e78b6267d3f05e72f4d63
Kangaru <46432989+Kangaru94@users.noreply.github.com>
2020-03-11 16:27:46 -0500

    Add files via upload

---
+++ BestiaryInC#.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Console_Bestiary
+{
+    class Program
+    {
+        static void Main()
+        {
+            Hello();
+            switchCase();
+        }
+
+        static void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void switchCase()
+        {
+            //string choice = "A";
+            string choice = Console.ReadLine();
+
+            string chosen = " ";
+
+            switch (choice)
+            {
+                case "A":
+                case "a":
+                    chosen = "You chose to attack";
+                    break;
+
+                case "B":
+                case "b":
+                    chosen = "You chose to block";
+                    break;
+            }
+
+            Console.WriteLine(chosen);
+            
+        }
+    }
+}
